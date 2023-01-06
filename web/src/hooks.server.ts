3dbb3a19fbc07d523fdb44a75866ad0ecf509d45
NafDev <54936211+NafDev@users.noreply.github.com>
2023-01-06 18:51:34 +0000

    .

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -1,15 +1,15 @@
+import { BASE_API_URL } from '$/lib/config';
 import { redirect, type HandleFetch } from '@sveltejs/kit';
-import { BASE_API_URL } from '$/lib/config'
 
-export const handleFetch = (({ request, event, fetch }) => {
+export const handleFetch: HandleFetch = ({ request, event, fetch }) => {
 	// `request` refers to the API endpoint being called in the load function
 	// `event` refers to the webpage url the client is trying to load
 
-  if (request.url.startsWith(BASE_API_URL)) {
+	if (request.url.startsWith(BASE_API_URL)) {
 		if (event.cookies.get('sFrontToken')?.length > 0 && !event.cookies.get('sAccessToken')) {
-			throw redirect(302, `/users/refresh-session?redirect=${encodeURIComponent(event.url.href)}`)
+			throw redirect(302, `/users/refresh-session?redirect=${encodeURIComponent(event.url.href)}`);
 		}
-  }
+	}
 
-  return fetch(request);
-}) satisfies HandleFetch;
+	return fetch(request);
+};
